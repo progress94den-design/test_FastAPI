1b58d6804203f352679c44cad1f0d779edcb7de3
progress94den-deisgn <progress94den@gmail.com>
2025-12-29 16:30:26 +0300

    create users router

--- src/api/api_v1/users.py
+++ src/api/api_v1/users.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter
 
+from api.dependencies.authentication.fastapi_users_routers import fastapi_users
+from schemas.user import UserRead, UserUpdate
+
 users_router = APIRouter()
 
+# /me
+# /{id}
+users_router.include_router(
+    router=fastapi_users.get_users_router(UserRead, UserUpdate),
+)
 
-@users_router.get("/")
-async def index():
-    return {"message": "Hello World"}
+
+@users_router.delete("/{id}")
+def delete_user(id: str) -> dict[str, str]:
+    return {
+        "message": "Метод не доступен",
+    }
