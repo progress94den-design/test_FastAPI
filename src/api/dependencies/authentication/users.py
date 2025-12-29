44dc59c943a059450dd777ffe6aa5022cf63496c
progress94den-deisgn <progress94den@gmail.com>
2025-12-29 15:51:32 +0300

    fix annotated

--- src/api/dependencies/authentication/users.py
+++ src/api/dependencies/authentication/users.py
@@ -1,4 +1,5 @@
 from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated, AsyncGenerator
 
@@ -11,5 +12,5 @@ async def get_users_db(
         AsyncSession,
         Depends(db_helper.session_getter),
     ],
-) -> AsyncGenerator[AsyncSession, None]:
+) -> AsyncGenerator[SQLAlchemyUserDatabase, None]:
     yield User.get_db(session=session)
