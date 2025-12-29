2ff0d296d7e45bc5b466b71e483392ad1326c5ac
progress94den-deisgn <progress94den@gmail.com>
2025-12-29 14:15:54 +0300

    add fastapi_users

---
+++ src/api/dependencies/authentication/fastapi_users_routers.py
@@ -0,0 +1,18 @@
+import uuid
+
+from fastapi_users import FastAPIUsers
+
+from models.users import User
+
+from api.dependencies.authentication.user_manager import get_user_manager
+from api.dependencies.authentication.auth_backend import authentication_backend
+
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](
+    get_user_manager,
+    [authentication_backend],
+)
+
+current_active_user = fastapi_users.current_user(active=True)
+current_anon_user = fastapi_users.current_user()
+current_active_superuser = fastapi_users.current_user(active=True, superuser=True)
