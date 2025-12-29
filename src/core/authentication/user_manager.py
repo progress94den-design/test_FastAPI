2e19391cd6bb3f5100c0c8b68af6e7d110737918
progress94den-deisgn <progress94den@gmail.com>
2025-12-29 17:30:16 +0300

    fix model User and UserManager

--- src/core/authentication/user_manager.py
+++ src/core/authentication/user_manager.py
@@ -1,15 +1,38 @@
 import uuid
 
-from fastapi import Depends, Request
+from fastapi import Request
 from fastapi_users import BaseUserManager, UUIDIDMixin
+from fastapi import HTTPException
+from sqlalchemy import select
 
 from models.users import User
 from core.config import settings
+from schemas.user import UserCreate
 
 
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     reset_password_token_secret = settings.cookie.reset_password_token_secret
     verification_token_secret = settings.cookie.verification_token_secret
+
+    async def get_by_phone(self, phone: str) -> User | None:
+        stmt = select(User).where(User.phone_number == phone)
+        result = await self.user_db.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def create(
+        self,
+        user_create: UserCreate,
+        safe: bool = False,
+        request: Request | None = None,
+    ):
+        user = await self.get_by_phone(user_create.phone_number)
+        if user:
+            raise HTTPException(
+                status_code=400,
+                detail="Пользователь с таким номером телефона уже существует",
+            )
+
+        return await super().create(user_create, safe, request)
 
     async def on_after_register(
         self,
@@ -17,13 +40,3 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
         request: Request | None = None,
     ) -> None:
         print(f"User {user.id} has registered.")
-
-    # async def on_after_forgot_password(
-    #     self, user: User, token: str, request: Request | None = None
-    # ):
-    #     print(f"User {user.id} has forgot their password. Reset token: {token}")
-    #
-    # async def on_after_request_verify(
-    #     self, user: User, token: str, request: Request | None = None
-    # ):
-    #     print(f"Verification requested for user {user.id}. Verification token: {token}")
--- src/models/users.py
+++ src/models/users.py
@@ -1,5 +1,5 @@
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.base import Base
@@ -8,7 +8,7 @@ from models.base import Base
 class User(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = "users"
 
-    phone_number: Mapped[str]
+    phone_number: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
 
     @classmethod
     def get_db(cls, session: AsyncSession) -> SQLAlchemyUserDatabase:
