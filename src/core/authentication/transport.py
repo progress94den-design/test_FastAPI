fb1ed4c39cfaef6c18a61212a1f97d8c49ce72b8
progress94den-deisgn <progress94den@gmail.com>
2025-12-29 13:57:41 +0300

    add transport for cookie

--- src/core/authentication/transport.py
+++ src/core/authentication/transport.py
@@ -1,3 +1,7 @@
 from fastapi_users.authentication import CookieTransport
 
-cookie_transport = CookieTransport(cookie_max_age=3600)
+from core.config import settings
+
+cookie_transport = CookieTransport(
+    cookie_max_age=settings.cookie.cookie_max_age,
+)
