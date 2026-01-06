f395ec60d528271e18005b8d8565d23e1c340ee6
progress94den-deisgn <progress94den@gmail.com>
2026-01-07 00:35:44 +0300

    added in UserManager send welcome mail on_after_register

--- src/core/authentication/user_manager.py
+++ src/core/authentication/user_manager.py
@@ -8,6 +8,7 @@ from sqlalchemy import select
 from models.users import User
 from core.config import settings
 from schemas.user import UserCreate
+from services.mailing.send_welcome import send_welcome
 
 
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
@@ -39,4 +40,5 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
         user: User,
         request: Request | None = None,
     ) -> None:
+        await send_welcome(recipient=user.email)
         print(f"User {user.id} has registered.")
