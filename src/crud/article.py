456d10ecc81ed820c1dcd4a0d064e8566c874f4e
progress94den-deisgn <progress94den@gmail.com>
2026-01-16 22:08:12 +0300

    refactor code

--- src/crud/article.py
+++ src/crud/article.py
@@ -1,6 +1,5 @@
 from sqlalchemy import select, func
 from sqlalchemy.sql import Select
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from crud.base import CRUDBase
 from models.article import Article
